682dd0d5766bfed45375bdf36db7ff1bbf921b99
Thomas Galliker <thomas@bluewin.ch>
2018-09-15 09:56:55 +0200

    Initial commit

---
+++ DebugInCode/Person.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace DebugInCode
+{
+    partial class Program
+    {
+        [DebuggerDisplay("Person: Id={this.Id}, Relatives={this.Relatives.Count}")]
+        public class Person
+        {
+            public Person()
+            {
+                this.Relatives = new List<Person>();
+            }
+
+            public int Id { get; set; }
+
+            public ICollection<Person> Relatives { get; set; }
+        }
+    }
+}
---
+++ DebugInCode/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace DebugInCode
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            //if (!Debugger.IsAttached)
+            //{
+            //    Debugger.Launch();
+            //}
+
+            //Debugger.Break();
+
+            if (Debugger.IsAttached)
+            {
+                Console.WriteLine("Debugger is attached!");
+            }
+            else
+            {
+                Console.WriteLine("Debugger is not attached");
+            }
+
+            // DEMO: DebuggerVisible
+            var persons = Enumerable.Range(0, 1000)
+                .Select(i => new Person { Id = i, Relatives = Enumerable.Range(0, 1000).Select(x => new Person { Id = x }).ToList() })
+                .ToList();
+
+        }
+    }
+}
